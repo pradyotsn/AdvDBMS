7263621e9cce4a9fa582a15b4b269fc7ed8465d5
pradyotsn <pradyotsn@gmail.com>
2020-02-07 01:00:29 +0530

    qq
    qq

--- DBMS_Project/src/com/mit/OracleCon.java
+++ DBMS_Project/src/com/mit/OracleCon.java
@@ -9,15 +9,18 @@ Class.forName("oracle.jdbc.driver.OracleDriver");
   
 //step2 create  the connection object  
 Connection con=DriverManager.getConnection(  
-"jdbc:oracle:thin:@localhost:1521:orcl_MIT","system","oracle");  
+"jdbc:oracle:thin:@localhost:1521:orclMIT","c##prady","oracle");  
   
+System.out.println("connection created");
 //step3 create the statement object  
 Statement stmt=con.createStatement();  
   
 //step4 execute query  
-ResultSet rs=stmt.executeQuery("select * from dual");  
+ResultSet rs=stmt.executeQuery("\r\n" + 
+		"select * from   employee\r\n" + 
+		"");  
 while(rs.next())  
-System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+System.out.println(rs.getString(1)+" \t| "+rs.getString(2)+" \t| "+rs.getString(3));  
   
 //step5 close the connection object  
 con.close();  
