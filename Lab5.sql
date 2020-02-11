c8b5d8c461ea4336ffada1ac53855b401a286f04
pradyotsn <pradyotsn@gmail.com>
2020-02-11 08:46:58 +0530

    Create Lab5.sql

---
+++ Lab5.sql
@@ -0,0 +1,9 @@
+Create view course_sections as select course.title, section.building, section.room_number from course, section where course.course_id=section.course_id AND course.dept_name='Physics' AND section.semester='Fall' AND section.year=2009;
+select title FROM COURSE_SECTIONS;
+Create view dept_tot_sal as select department.Dname, sum(employee1.salary) AS tot_salary from employee1, department where employee1.Dno=department.Dnumber group by department.Dname;
+Create view instructor_info as select instructor.ID, instructor.name, section.building from instructor,section,teaches where instructor.ID=teaches.ID and teaches.course_id=section.course_id;
+create view history_instructors as select instructor.ID, instructor.name,instructor.dept_name,instructor.salary, teaches.Course_id,teaches.sec_id, teaches.semester, teaches.year, section.building, section.room_number, section.time_slot_id from instructor, teaches, section where instructor.ID=teaches.ID AND teaches.course_id=section.course_id and instructor.dept_name='History';
+ CREATE VIEW DPT1 AS SELECT DNAME,FNAME AS MGR_FNAME,LNAME AS MGR_LNAME,SALARY FROM DEPT,EMPLOYEE WHERE Mgr_ssn=Ssn;
+CREATE VIEW EMP2 AS SELECT E1.FNAME,E1.LNAME,E2.FNAME AS S_FNAME,E2.LNAME AS S_LNAME,E1.SALARY FROM EMPLOYEE E1, EMPLOYEE E2, DEPT D WHERE E1.Super_ssn=E2.Ssn AND E1.DNO = D.DNUMBER AND D.DNAME='Research';
+CREATE VIEW PROJ3 AS SELECT PNUMBER,DNAME,COUNT(*),SUM(Hours) from PROJECT,WORKS_ON,DEPT WHERE PNUMBER = PNO and DNUM = DNUMBER GROUP BY PNUMBER,PNAME,DNAME;
+CREATE VIEW PROJ4 AS SELECT PNUMBER,DNAME,COUNT(*),SUM(Hours) from PROJECT,WORKS_ON,DEPT WHERE PNUMBER = PNO and DNUM = DNUMBER GROUP BY PNUMBER,PNAME,DNAME having count(*)>1; 
